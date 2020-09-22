6d8dba7198ae544a348fc970f173a5b979432c75
Kairi Ido <kaairii0913@gmail.com>
2020-09-22 12:50:37 +0900

    Initial Commit

---
+++ TapNamber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TapNamber
+//
+//  Created by 井戸海里 on 2020/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
